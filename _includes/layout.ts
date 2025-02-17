df65cab3adf780bb85ae2988d61127b3ea6b49c2
Falco Gerritsjans <alias.fcb319@falco.pro>
2025-02-17 18:12:15 +0100

    fix: move style to head

--- _includes/layout.ts
+++ _includes/layout.ts
@@ -3,28 +3,28 @@ export default ({ content, season }: Lume.Data, helpers: Lume.Helpers) =>
 	<head>
 		<title>Is het zomer? - ${season.current === "summer"? "ja!" : "nee!"}</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<style>
+			html {
+				background: linear-gradient(azure, lightblue);
+				min-height: 100vh;
+				display: flex;
+			}
+			
+			html[data-season="summer"] {
+				background: linear-gradient(darkorange, gold)
+			}
+			
+			body {
+				max-width: 32em;
+				margin: 0 auto;
+				font-family: sans-serif;
+				color: black;
+				line-height: 1.5em;
+				text-align: center;		
+				align-self: center;
+			}
+		</style>
 	</head>
-	<style>
-		html {
-			background: linear-gradient(azure, lightblue);
-			min-height: 100vh;
-			display: flex;
-		}
-		
-		html[data-season="summer"] {
-			background: linear-gradient(darkorange, gold)
-		}
-		
-		body {
-			max-width: 32em;
-			margin: 0 auto;
-			font-family: sans-serif;
-			color: black;
-			line-height: 1.5em;
-			text-align: center;		
-			align-self: center;
-		}
-	</style>
 	<body>
 	${content}
 	</body>
